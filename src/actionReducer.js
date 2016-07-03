1102da97bdd0140e67d0d12773156cf48cc25891
VIctor Alvarez <v.alvarez312@gmail.com>
2016-07-03 10:02:17 -0700

    jsdoc

--- src/actionReducer.js
+++ src/actionReducer.js
@@ -4,6 +4,16 @@ import identity from 'lodash.identity'
 
 import isFSAReducer from './isFSAReducer'
 
+/**
+ * Returns a reducer that handles actions of a given type.
+ *
+ * @param {string} type the action type; usually an uppercased and snake-cased string
+ * @param {function} reducer a function of non-zero arity, or undefined (in this case the identity function is used)
+ *                           this may also be an object with next and throw reducers, which each handle non-error
+ *                           and error actions, respectively
+ * @param {object} defaultState a plain object; the state to use when the reducer is called with undefined state
+ * @returns {function} the resulting reducer
+ */
 export default function makeActionReducer(type, reducer = identity, defaultState) {
   if (!isFSAReducer(reducer)) {
     throw new TypeError('Wrong shape for reducer argument, use a function or object with next/throw reducers.')
--- src/actionReducers.js
+++ src/actionReducers.js
@@ -1,5 +1,15 @@
 import makeActionReducer from './actionReducer'
 
+/**
+ * Takes a map of action types to reducers, and returns a reducer that operates
+ * by using all these reducers. Note that the calling order of the reducers in
+ * reducersMap is not guaranteed.
+ *
+ * @param {object} reducersMap a map of action types to reducers, which is either an
+ *                             object with next/throw reducers or is itself a reducer
+ * @param {object} defaultState the default state for the resulting reducer
+ * @returns {function} a reduction of the reducers in reducersMap
+ */
 export default function makeActionReducers(reducersMap, defaultState) {
   const actionReducers = Object
     .keys(reducersMap)
