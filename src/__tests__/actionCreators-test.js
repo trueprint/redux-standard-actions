7c91736e5da495a44f96361874b84f9e0515c863
VIctor Alvarez <v.alvarez312@gmail.com>
2016-07-04 00:14:56 -0700

    clean tests

--- src/__tests__/actionCreators-test.js
+++ src/__tests__/actionCreators-test.js
@@ -5,99 +5,99 @@ describe('makeActionCreators', () => {
     const expectedError = 'Expected (optional) object followed by string action types'
 
     expect(() => makeActionCreators(1)).to.throw(TypeError, expectedError)
-    expect(() => makeActionCreators({
-      ACTION_ONE: undefined,
-    }, [])).to.throw(TypeError, expectedError)
-    expect(() => makeActionCreators('ACTION_ONE', true)).to.throw(TypeError, expectedError)
+    expect(() => makeActionCreators({ ACTION_1: undefined }, [])).to.throw(TypeError, expectedError)
+    expect(() => makeActionCreators('ACTION_1', true)).to.throw(TypeError, expectedError)
   })
 
   it('should throw an error when given bad payload creators', () => {
-    expect(() => makeActionCreators({
-      ACTION_ONE: [],
-    })).to.throw(TypeError, 'Expected function or undefined payload creator for ACTION_ONE')
+    expect(
+      () => makeActionCreators({ ACTION_1: [] })
+    ).to.throw(TypeError, 'Expected function or undefined payload creator for ACTION_1')
 
-    expect(() => makeActionCreators({
-      ACTION_ONE: undefined,
-      ACTION_TWO: 'string',
-    })).to.throw(TypeError, 'Expected function or undefined payload creator for ACTION_TWO')
+    expect(
+      () => makeActionCreators({
+        ACTION_1: undefined,
+        ACTION_2: 'string',
+      })
+    ).to.throw(TypeError, 'Expected function or undefined payload creator for ACTION_2')
   })
 
   it('should return a map of camel-cased action types to action creators', () => {
-    const { actionOne, actionTwo } = makeActionCreators({
-      ACTION_ONE(key, value) {
+    const { action1, action2 } = makeActionCreators({
+      ACTION_1(key, value) {
         return { [key]: value }
       },
-      ACTION_TWO(first, second) {
+      ACTION_2(first, second) {
         return [ first, second ]
       },
     })
 
-    expect(actionOne('key', 1)).to.deep.equal({
-      type: 'ACTION_ONE',
-      payload: { key: 1 },
+    expect(action1('from', 1)).to.deep.equal({
+      type: 'ACTION_1',
+      payload: { from: 1 },
     })
-    expect(actionTwo('first', 'second')).to.deep.equal({
-      type: 'ACTION_TWO',
-      payload: [ 'first', 'second' ],
+    expect(action2('from', 2)).to.deep.equal({
+      type: 'ACTION_2',
+      payload: [ 'from', 2 ],
     })
   })
 
-  it('should use the identity payload creator if the map value is not a function', () => {
-    const { actionOne, actionTwo } = makeActionCreators({
-      ACTION_ONE: undefined,
-      ACTION_TWO: undefined,
+  it('should use the identity payload creator if the map reducer value is not a function', () => {
+    const { action1, action2 } = makeActionCreators({
+      ACTION_1: undefined,
+      ACTION_2: undefined,
     })
 
-    expect(actionOne('payload')).to.deep.equal({
-      type: 'ACTION_ONE',
-      payload: 'payload',
+    expect(action1(1)).to.deep.equal({
+      type: 'ACTION_1',
+      payload: 1,
     })
 
-    expect(actionTwo({ two: 2 })).to.deep.equal({
-      type: 'ACTION_TWO',
-      payload: { two: 2 },
+    expect(action2({ from: 2 })).to.deep.equal({
+      type: 'ACTION_2',
+      payload: { from: 2 },
     })
   })
 
   it('should use identity payload creators if given string action types', () => {
-    const { actionOne, actionTwo } = makeActionCreators('ACTION_ONE', 'ACTION_TWO')
+    const { action1, action2 } = makeActionCreators('ACTION_1', 'ACTION_2')
 
-    expect(actionOne('payload')).to.deep.equal({
-      type: 'ACTION_ONE',
-      payload: 'payload',
+    expect(action1(1)).to.deep.equal({
+      type: 'ACTION_1',
+      payload: 1,
     })
 
-    expect(actionTwo(1)).to.deep.equal({
-      type: 'ACTION_TWO',
-      payload: 1,
+    expect(action2(2)).to.deep.equal({
+      type: 'ACTION_2',
+      payload: 2,
     })
   })
 
   it('should create actions from an object and action types', () => {
-    const { actionOne, actionTwo, actionThree, actionFour } = makeActionCreators({
-      ACTION_ONE(key, value) {
+    const { action1, action2, action3, action4 } = makeActionCreators({
+      ACTION_1(key, value) {
         return { [key]: value }
       },
-      ACTION_TWO(first, second) {
+      ACTION_2(first, second) {
         return [ first, second ]
       },
-    }, 'ACTION_THREE', 'ACTION_FOUR')
+    }, 'ACTION_3', 'ACTION_4')
 
-    expect(actionOne('key', 1)).to.deep.equal({
-      type: 'ACTION_ONE',
-      payload: { key: 1 },
+    expect(action1('from', 1)).to.deep.equal({
+      type: 'ACTION_1',
+      payload: { from: 1 },
     })
-    expect(actionTwo('first', 'second')).to.deep.equal({
-      type: 'ACTION_TWO',
-      payload: [ 'first', 'second' ],
+    expect(action2('from', 2)).to.deep.equal({
+      type: 'ACTION_2',
+      payload: [ 'from', 2 ],
     })
-    expect(actionThree('payload')).to.deep.equal({
-      type: 'ACTION_THREE',
-      payload: 'payload',
+    expect(action3(3)).to.deep.equal({
+      type: 'ACTION_3',
+      payload: 3,
     })
-    expect(actionFour(1)).to.deep.equal({
-      type: 'ACTION_FOUR',
-      payload: 1,
+    expect(action4(4)).to.deep.equal({
+      type: 'ACTION_4',
+      payload: 4,
     })
   })
 })
